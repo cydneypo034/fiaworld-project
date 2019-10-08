8ff8ab8deaa6ab233470c6819c3c14d0397a223e
Cydney Pollard <sincerelycydney087@gmail.com>
2019-10-08 09:33:29 -0400

    changed route names

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import home from './components/home.js';
-import about from './components/about.js';
-import createartist from './components/createartist.js'
-import musicartists from './components/artists.js';
-import movies from './components/movies.js';
-import aboutprod from './components/aboutproducer.js';
-import talkshows from './components/talkshows.js';
-import contact from './components/contact.js';
+import Home from './components/home.js';
+import About from './components/about.js';
+import CreateArtist from './components/createartist.js'
+import MusicArtists from './components/artists.js';
+import Movies from './components/movies.js';
+import AboutProd from './components/aboutproducer.js';
+import TalkShows from './components/talkshows.js';
+import Contact from './components/contact.js';
 import NavigationBar from './components/navigationbar.js';
 import Footer from './components/footer.js'
 
@@ -19,14 +19,14 @@ class App extends React.Component {
         <Router>
           <NavigationBar />
           <Switch>
-            <Route path="/" exact component={home} />
-            <Route path="/about" component={about} />
-            <Route path="/aboutprod" component={aboutprod} />
-            <Route path="/createartist" component={createartist} />
-            <Route path="/artists" component={musicartists} />
-            <Route path="/movies" component={movies} />
-            <Route path="/talkshows" component={talkshows} />
-            <Route path="/contact" component={contact} />
+            <Route path="/" exact component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/aboutprod" component={AboutProd} />
+            <Route path="/createartist" component={CreateArtist} />
+            <Route path="/artists" component={MusicArtists} />
+            <Route path="/movies" component={Movies} />
+            <Route path="/talkshows" component={TalkShows} />
+            <Route path="/contact" component={Contact} />
           </Switch>
           <Footer />
         </Router>
