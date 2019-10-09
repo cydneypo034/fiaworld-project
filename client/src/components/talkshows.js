23b12f891c5e19a1f34feb94fbbc2b4dfff7c3a9
Cydney Pollard <sincerelycydney087@gmail.com>
2019-10-09 10:28:10 -0400

    finished talk show component

--- client/src/components/talkshows.js
+++ client/src/components/talkshows.js
@@ -1,5 +1,5 @@
 import React from 'react';
-//import {Card} from 'react-bootstrap';
+import {Card} from 'react-bootstrap';
 
 class TalkShows extends React.Component {
     
@@ -7,7 +7,7 @@ class TalkShows extends React.Component {
         super(props)
        
         this.state = {
-            talkshows: []
+            shows: []
             };
     }
     
@@ -17,26 +17,27 @@ class TalkShows extends React.Component {
         .then(res => res.json())
         .then(currentShow=> {
             console.log('here are the current shows:', currentShow)
-            this.setState({talkshows: currentShow})
+            this.setState({shows: currentShow})
         })
     }
 
-    // showList() {
-    //     const Artist = props => (
-    //         <Card style={{width: '18rem', height: '37rem'}} >
-    //             <Card.Img variant='top' src={props.artist.artist_image} alt="artist" className="artist-image"/>
-    //             <Card.Body>
-    //                 <Card.Text className="about-text">
-    //                     {props.artist.name} <br />
-    //                     {props.artist.biography}
-    //                 </Card.Text>
-    //             </Card.Body>
-    //         </Card>
-    //     )
-    //     return this.state.artists.map(function(currentArtist, i){
-    //         return <Artist artist={currentArtist} key={i} />
-    //     });
-    // }
+    showList() {
+         const Show = props => (
+             <Card style={{width: '21rem', height: '37rem'}} >
+                 <Card.Img variant='top' src={props.show.show_image} alt="artist" className="artist-image"/>
+                 <Card.Body>
+                     <Card.Text className="about-text">
+                         {props.show.name} <br />
+                         {props.show.description} <br />
+                         {props.show.hosts}
+                     </Card.Text>
+                 </Card.Body>
+             </Card>
+         )
+         return this.state.shows.map(function(currentShow, i){
+             return <Show show={currentShow} key={i} />
+         });
+     }
 
     render() {
 
@@ -50,7 +51,7 @@ class TalkShows extends React.Component {
                 and relationship advice.</p>
             <div className="card-wrapper">
 
-               
+               {this.showList()}
                 </div>
                 </div>
         )
