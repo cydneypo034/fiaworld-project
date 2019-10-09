f5dbaa7064de5fc6fdf0bcdcea84270f9e760a87
Cydney Pollard <sincerelycydney087@gmail.com>
2019-10-09 09:49:57 -0400

    added talk show components

--- client/src/components/artists.js
+++ client/src/components/artists.js
@@ -56,41 +56,7 @@ class Artists extends React.Component {
                 </div>
         )
     }
-//'http://localhost:8000/media/uploads/Screen_Shot_2019-10-06_at_7.56.35_PM.png'
 }
 
 export default Artists;
 
-/*        var { artists } = this.state;
-
- <Card style={{width: '18rem'}}>
-                   
-                   {artists.map(artist => (
-                       <div key={artist.id}>
-                           Name: {artist.name} 
-                           | Biography: {artist.biography} 
-                           | Image: <img src={artist.artist_image} alt="artist"/>
-                       </div>
-                   ))}
-       
-                </Card>
-artistList() {
-        const Artist = props => (
-         <div className="card">
-             <img src={this.props.artist_image} alt="artist candid"/>
-             <div className="card-body">
-                 <div className="title">{props.artist.name}</div>
-             <div className="content">
-                 Name: {props.artist.name} <br></br>
-                 Biography: {props.artist.biography} <br></br>
-                 Artist Image: {props.artist.artist_image} <br></br>
-             </div>
-                
-         </div>
-         </div>
- ) 
-         return this.state.artists.map(function(currentArtist, i){
-             return <Artist artist={currentArtist} key={i} />;
-         });
-     }
-*/
--- client/src/components/talkshows.js
+++ client/src/components/talkshows.js
@@ -1,12 +1,62 @@
 import React from 'react';
+//import {Card} from 'react-bootstrap';
 
 class TalkShows extends React.Component {
-    render() {
-        return (
-            <div className="about-text">Wassup</div>
-        )
+    
+    constructor(props){
+        super(props)
+       
+        this.state = {
+            talkshows: []
+            };
+    }
+    
+   
+    componentDidMount = () => {
+        fetch('/api/shows/')
+        .then(res => res.json())
+        .then(currentShow=> {
+            console.log('here are the current shows:', currentShow)
+            this.setState({talkshows: currentShow})
+        })
     }
 
+    // showList() {
+    //     const Artist = props => (
+    //         <Card style={{width: '18rem', height: '37rem'}} >
+    //             <Card.Img variant='top' src={props.artist.artist_image} alt="artist" className="artist-image"/>
+    //             <Card.Body>
+    //                 <Card.Text className="about-text">
+    //                     {props.artist.name} <br />
+    //                     {props.artist.biography}
+    //                 </Card.Text>
+    //             </Card.Body>
+    //         </Card>
+    //     )
+    //     return this.state.artists.map(function(currentArtist, i){
+    //         return <Artist artist={currentArtist} key={i} />
+    //     });
+    // }
+
+    render() {
+
+
+        return (
+            <div >
+                <h1 className="home-text3">Listen To Our Talk Shows</h1>
+                <p className="para-center">The FIA Broadcasting Network has 
+                produced several talk shows since 2009. <br/> Below are a few of the 
+                talk shows we've produced that cover topics such as marriage, entertainment in Atlanta 
+                and relationship advice.</p>
+            <div className="card-wrapper">
+
+               
+                </div>
+                </div>
+        )
+    }
 }
+
+
 
 export default TalkShows;
