bcc6c4c8337934665b5c647c1db19f59ea957d87
Cydney Pollard <sincerelycydney087@gmail.com>
2019-10-10 14:14:53 -0400

    added user

--- client/src/components/createartist.js
+++ client/src/components/createartist.js
@@ -2,86 +2,96 @@ import React from 'react';
 
 class CreateArtist extends React.Component {
 
-    constructor(props){
+    constructor(props) {
         super(props)
-            
+
         this.state = {
             name: '',
             biography: '',
-            artist_image: ''
         }
 
         this.onChangeName = this.onChangeName.bind(this)
         this.onChangeBiography = this.onChangeBiography.bind(this)
-        this.onChangeArtistImage = this.onChangeArtistImage.bind(this)
+        //this.onChangeArtistImage = this.onChangeArtistImage.bind(this)
     }
 
     onChangeName(e) {
         this.setState({
             name: e.target.value
         })
-        console.log("name changed!")
     }
 
     onChangeBiography(e) {
         this.setState({
             biography: e.target.value
         })
-        console.log("bio changed!")
     }
 
-    onChangeArtistImage(e) {
-        this.setState({
-            artist_image: e.target.value
-        })
-        console.log('picture selected!')
-    }
+    // onChangeArtistImage(e) {
+    //     this.setState({
+    //         artist_image: e.target.value
+    //     })
+    //     console.log('picture selected!')
+    // }
 
 
-    onSubmit(e) {
+    onSubmit = (e) => {
         e.preventDefault();
 
-            
+        console.log("Form Submitted")
 
+        const newArtist = {
+            name: this.state.name,
+            biography: this.state.biography,
+        }
 
+        fetch('/api/artists/', {
+            headers: { "Content-Type": "application/json"},
+            body: JSON.stringify(newArtist),
+            method: "POST"
+        })
 
-        
+            //Axios.post("api/artists/", newArtist)
+            // .then(res => console.log(res.data))
 
+        this.setState({
+            name: '',
+            biography: '',
+        })
 
-
+        this.props.history.push('/artists');
     }
-
-
-
-
-
-
- 
 
     render() {
         return (
             <div className="about-body">
-            <section className="hero is-medium has-bg-img">
+                <section className="hero is-medium has-bg-img">
 
-            <div className="hero-body">
-                <div className="container">
-                <h3 className="home-text3">Create New Artist</h3>
-                <p className="para-center"> For a New Artist to be made, fill out the form below and 
+                    <div className="hero-body">
+                        <div className="container">
+                            <h3 className="home-text3">Create New Artist</h3>
+                            <p className="para-center"> For a New Artist to be made, fill out the form below and
                                 it will be added to the artist list!</p>
-                </div>
-                </div>
-            </section>
-                            
+                        </div>
+                    </div>
+                </section>
+
                 <form onSubmit={this.onSubmit}>
-                    <input type="text" name="name" onChange={this.onChangeName} placeholder="Enter Name" />
-                    <input type="text" name="biography" onChange={this.onChangeBiography} placeholder="Enter bio" />
-                    <input type="file" name="artist_image" onChange={this.onChangeArtistImage} placeholder="choose file" />
+                    <input type="text" value={this.state.name}
+                        onChange={this.onChangeName} placeholder="Enter Name" />
+
+                    <br />
+                    <br />
+                    <input type="text" value={this.state.biography}
+                        onChange={this.onChangeBiography} placeholder="Enter bio" />
+                    <br />
+                    <br />
 
                     <input type="submit" name="submit" value="submit" />
                 </form>
 
-               
-                        </div>
+
+            </div>
 
         )
     }
